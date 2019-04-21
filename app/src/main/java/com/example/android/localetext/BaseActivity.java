c56bba0a790064201eb278927d724b93cff7b342
Prakash172 <electrophile172@gmail.com>
2019-04-21 12:35:17 +0530

    Default is english but you can switch between english and french

---
+++ app/src/main/java/com/example/android/localetext/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.android.localetext;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(LocaleHelper.onAttach(newBase));
+    }
+}
